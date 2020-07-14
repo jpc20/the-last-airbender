14047c367cc30d1d934b17d8d5a2dbc9ce5c8866
Jack Cullen <jpcullen20@gmail.com>
2020-07-14 09:31:50 -0600

    set up test for viewing the number of people in a nation

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-describe 'From the home page' do
-  it "user can select a nation" do
+describe 'Search index'do
+  it 'shows total number of people who live in a nation' do
     visit root_path
     page.select 'Fire Nation'
     click_on 'Search For Members'
     expect(current_path).to eq(search_path)
+    expect(page).to have_content('Total Number of people in the Fire Nation: 20')
   end
 end
