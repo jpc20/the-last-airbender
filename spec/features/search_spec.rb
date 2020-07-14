1920ba27db75cb16a69bfc4b6ce5a5ba9473ebd8
Jack Cullen <jpcullen20@gmail.com>
2020-07-14 09:23:31 -0600

    Set up route to the search idex

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-describe '' do
-
+describe 'From the home page' do
+  it "user can select a nation" do
+    visit root_path
+    page.select 'Fire Nation'
+    click_on 'Search For Members'
+    expect(current_path).to eq(search_path)
+  end
 end
