d092a627236ab9a64b2a00886d38fe1864bca4a1
Jack Cullen <jpcullen20@gmail.com>
2020-07-14 10:02:35 -0600

    Set up test for member list

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -8,4 +8,17 @@ describe 'Search index'do
     expect(current_path).to eq(search_path)
     expect(page).to have_content('Total Number of people in the Fire Nation: 20')
   end
+
+  it "shows all members information" do
+    visit root_path
+    page.select 'Fire Nation'
+    click_on 'Search For Members'
+    within '.members' do
+      expect(page).to have_css('.member', count: 20)
+      within(first('.member')) do
+        expect(page.to have_content('Name: Afiko'))
+        expect(page.to have_content('Affiliation: Fire Nation'))
+      end
+    end
+  end
 end
